f6029ba1207e85016471b31b96a375517ec2f052
Rahitya Velpuri <33275106+rahitya-123@users.noreply.github.com>
2024-10-31 23:18:41 -0400

    Add files via upload

---
+++ pre_processing_functions.sql
@@ -0,0 +1,41 @@
+/*create a database and a schema*/
+
+--CREATE DATABASE If NOT EXISTS CC_QUICKSTART_CORTEX_SEARCH_DOCS;
+--CREATE SCHEMA If NOT EXISTS DATA;
+ 
+/*  Create a table function that will split text into chunks */
+
+create or replace function text_chunker(pdf_text string)
+returns table (chunk varchar)
+language python
+runtime_version = '3.9'
+handler = 'text_chunker'
+packages = ('snowflake-snowpark-python', 'langchain')
+as
+$$
+from snowflake.snowpark.types import StringType, StructField, StructType
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import pandas as pd
+
+class text_chunker:
+
+    def process(self, pdf_text: str):
+        
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size = 1512, #Adjust this as you see fit
+            chunk_overlap  = 256, #This let's text have some form of overlap. Useful for keeping chunks contextual
+            length_function = len
+        )
+    
+        chunks = text_splitter.split_text(pdf_text)
+        df = pd.DataFrame(chunks, columns=['chunks'])
+        
+        yield from df.itertuples(index=False, name=None)
+$$;
+
+
+/* Create a Stage with Directory Table where you will be uploading your documents */
+create or replace stage docs ENCRYPTION = (TYPE = 'SNOWFLAKE_SSE') DIRECTORY = ( ENABLE = true );
+
+
+ls @docs;
